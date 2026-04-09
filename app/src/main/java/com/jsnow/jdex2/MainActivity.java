d3bd9e929a190913ee8cf0cbed059577ff39649d
KuLiPai <yun.svip@gmail.com>
2026-04-09 18:57:02 +0800

    feat(Storage): 修改存储位置，/storage/emulated/0/Android/data/<package>/files，无root可访问，安卓高版本用零宽访问
    feat(Permission): 封装了一个权限管理器处理不同安卓版本的权限问题

--- app/src/main/java/com/jsnow/jdex2/MainActivity.java
+++ app/src/main/java/com/jsnow/jdex2/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.jsnow.jdex2.databinding.ActivityMainBinding;
@@ -14,89 +16,72 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private ActivityMainBinding binding;
-    private void writeConfigWithRoot(String content, String targetApp) {
-        if (targetApp.isEmpty() || !isTargetAppInstalled(targetApp)) {
-            Toast.makeText(this,
-                    "目标应用不存在：" + targetApp + "\n请确认包名是否正确且已安装",
-                    Toast.LENGTH_LONG).show();
-            return;
-        }
-        Process process = null;
-        DataOutputStream os = null;
+
+
+
+    private void writeConfig(String content, String targetApp) {
+        // 由于不是每个app都会自动创建/storage/emulated/0/Android/data/<package>，所以不判断
+//        if (targetApp.isEmpty() || !isTargetAppInstalled(targetApp)) {
+//            Toast.makeText(this,
+//                    "目标应用不存在：" + targetApp + "\n请确认包名是否正确且已安装",
+//                    Toast.LENGTH_LONG).show();
+//            return;
+//        }
+
         try {
-            File tempFile = new File(getFilesDir(), "config.properties");
-            FileOutputStream fos = new FileOutputStream(tempFile, false);
+
+            // 存储位置放在/storage/emulated/0/Android/data/<package>/files，用零宽漏洞绕过权限
+            String targetDir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/\u200bdata/"+targetApp+"/files/";
+            File targetDirFile = new File(targetDir);
+            // 创建多级文件夹
+            targetDirFile.mkdirs();
+            // 配置文件
+            File targetFile = new File(targetDir+"config.properties");
+
+            FileOutputStream fos = new FileOutputStream(targetFile, false);
             fos.write(content.getBytes());
             fos.flush();
             fos.close();
 
-            String targetDir = "/data/data/" + targetApp + "/files/";
-            String targetPath = targetDir + "config.properties";
 
-            process = Runtime.getRuntime().exec("su");
-            os = new DataOutputStream(process.getOutputStream());
-
-            os.writeBytes("mkdir -p " + targetDir + "\n");
-
-            os.writeBytes("cp " + tempFile.getAbsolutePath() + " " + targetPath + "\n");
-
-            os.writeBytes("chmod 666 " + targetPath + "\n");
-            os.writeBytes("chmod 771 " + targetDir + "\n");
-
-            os.writeBytes("chown $(stat -c '%U:%G' " + targetDir + ") " + targetPath + "\n");
-
-
-            os.writeBytes("exit\n");
-            os.flush();
-
-            int exitValue = process.waitFor();
-
-            if (exitValue == 0) {
-                Toast.makeText(this, "写入成功：" + targetPath, Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "写入失败，错误码: " + exitValue, Toast.LENGTH_LONG).show();
-            }
-
-            tempFile.delete();
+            Toast.makeText(this, "写入成功：" + targetFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
 
         } catch (Exception e) {
             e.printStackTrace();
+            Log.d("JDEX2",e.getMessage().toString());
             Toast.makeText(this, "Write config failed：" + e.getMessage(), Toast.LENGTH_LONG).show();
-        } finally {
-            try {
-                if (os != null) os.close();
-                if (process != null) process.destroy();
-            } catch (Exception ignored) {}
         }
     }
-    private boolean isTargetAppInstalled(String targetApp) {
-        Process process = null;
-        DataOutputStream os = null;
-        try {
-            process = Runtime.getRuntime().exec("su");
-            os = new DataOutputStream(process.getOutputStream());
 
-            // 用 test -d 判断目录是否存在，存在返回 0，不存在返回非 0
-            os.writeBytes("test -d /data/data/" + targetApp + "\n");
-            os.writeBytes("exit $?\n");
-            os.flush();
 
-            int exitValue = process.waitFor();
-            return exitValue == 0;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            try {
-                if (os != null) os.close();
-                if (process != null) process.destroy();
-            } catch (Exception ignored) {}
-        }
-    }
+//    private boolean isTargetAppInstalled(String targetApp) {
+//        Process process = null;
+//        DataOutputStream os = null;
+//        try {
+//            process = Runtime.getRuntime().exec("su");
+//            os = new DataOutputStream(process.getOutputStream());
+//
+//            // 用 test -d 判断目录是否存在，存在返回 0，不存在返回非 0
+//            os.writeBytes("test -d /data/data/" + targetApp + "\n");
+//            os.writeBytes("exit $?\n");
+//            os.flush();
+//
+//            int exitValue = process.waitFor();
+//            return exitValue == 0;
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        } finally {
+//            try {
+//                if (os != null) os.close();
+//                if (process != null) process.destroy();
+//            } catch (Exception ignored) {}
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,41 +90,59 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // 动态申请读写权限
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                },
-                1
-        );
+
+        // 封装了一个类用于权限检查和请求
+        StoragePermissionManager.checkAndRequestPermission(this);
 
 
-        binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                String targetApp = binding.editTextTextPersonName.getText().toString().trim();
-                String whiteList = binding.editTextTextPersonName2.getText().toString().trim();
-                String blackList = binding.editTextTextPersonName3.getText().toString().trim();
 
-                boolean hook = binding.switch2.isChecked();
-                boolean debugger = binding.switch1.isChecked();
-                boolean innerclassesFilter =binding.switch3.isChecked();
-                boolean invokeConstructors = binding.invoke.isChecked();
-                String content =
-                        "targetApp=" + targetApp + "\n" +
-                                "hook=" + hook + "\n" +
-                                "invokeDebugger=" + debugger + "\n" +
-                                "whiteList=" + whiteList + "\n" +
-                                "blackList=" + blackList + "\n" +
-                                "innerclassesFilter=" + innerclassesFilter + "\n" +
-                                "invokeConstructors=" + invokeConstructors + "\n";
+        binding.button.setOnClickListener(v -> {
 
-                // 没招了，高版本Android的读写权限太严了，只能用root了
-                writeConfigWithRoot(content, targetApp);
-            }
+            String targetApp = binding.editTextTextPersonName.getText().toString().trim();
+            String whiteList = binding.editTextTextPersonName2.getText().toString().trim();
+            String blackList = binding.editTextTextPersonName3.getText().toString().trim();
+
+            boolean hook = binding.switch2.isChecked();
+            boolean debugger = binding.switch1.isChecked();
+            boolean innerclassesFilter =binding.switch3.isChecked();
+            boolean invokeConstructors = binding.invoke.isChecked();
+            String content =
+                    "targetApp=" + targetApp + "\n" +
+                            "hook=" + hook + "\n" +
+                            "invokeDebugger=" + debugger + "\n" +
+                            "whiteList=" + whiteList + "\n" +
+                            "blackList=" + blackList + "\n" +
+                            "innerclassesFilter=" + innerclassesFilter + "\n" +
+                            "invokeConstructors=" + invokeConstructors + "\n";
+
+            // 没招了，高版本Android的读写权限太严了，只能用root了
+            // 全部改用了零宽漏洞绕过权限，至少目前我的安卓16还存在这个漏洞
+            writeConfig(content, targetApp);
         });
     }
+
+
+    /**
+     * 处理从设置页面返回的结果
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == StoragePermissionManager.REQUEST_CODE_ALL_FILES) {
+            // 用户从“所有文件访问权限”页面回来了
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    Toast.makeText(this, "权限已授予", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "未授予权限，无法读写文件", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
 }
+
+
